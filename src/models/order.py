a9b53cf38ca86ce763bbc3150d1c93633913cf37
Cruz Ortiz <cortiz99>
2019-12-05 19:38:43 -0400

    updating the project

--- src/models/order.py
+++
@@ -1,9 +0,0 @@
-class Order():
-    def __init__(self, burgers):
-        self.burgers = burgers
-
-    def get_order_price(self):
-        price = 0
-        for burger in self.burgers:
-            price += price + burger.price()
-        return price

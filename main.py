5956e54ea55f460911f957ea943712c8ad12147f
Cruz Ortiz <cortiz99>
2019-12-06 14:26:58 -0400

    updating

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from flask import Flask
 from config import Dev, Prod
-from src.routes.user import user_routes
-from src.routes.burger import burger_routes
 
 app = Flask(__name__)
-
-user_routes(app)
-burger_routes(app)
 
 
 if __name__ == '__main__':
--- src/repository/user.py
+++ src/repository/user.py
@@ -1,4 +1,7 @@
-class User():
+from repository.entities import Entity
+
+
+class User(Entity):
     def __init__(self, name, password):
         self.name = name
         self.password = password

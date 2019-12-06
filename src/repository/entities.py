2dcead31b0adb5141a90dafb1c31a3116f20299e
Cruz Ortiz <cortiz99>
2019-12-06 14:21:29 -0400

    updating

---
+++ src/repository/entities.py
@@ -0,0 +1,15 @@
+class Entity():
+    def save(self):
+        raise Exception("debe implementar este elemento")
+
+    def get_all(self):
+        raise Exception("debe implementar este elemento")
+
+    def get_by_id(self, id):
+        raise Exception("debe implementar este elemento")
+
+    def get_delete(self, id):
+        raise Exception("debe implementar este elemento")
+
+    def update(self, id, entity):
+        raise Exception("debe implementar este elemento")

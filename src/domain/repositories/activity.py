51b60ad6ccf3c91a4f218320d33af0a05c91e260
Cruz Ortiz <cortiz99>
2019-12-10 15:46:22 -0400

    activity repository done

--- src/domain/repositories/activity.py
+++ src/domain/repositories/activity.py
@@ -1,14 +1,49 @@
+from pathlib import Path
+from json import load, dump
+path = Path(__file__).parent.joinpath('..', 'db', 'activity.json')
+
+
 def save_activity(activity):
-    raise Exception('must be implemented')
+    json_file = open(path, 'r', encoding='utf-8')
+    json_activities = load(json_file)
+    json_file.close()
+    exists = len([json_activity for json_activity in json_activities if json_activity['email']
+                  == activity.email] and json_activity['id'] == activity.email) > 0
+    if exists:
+        raise Exception('registro existente')
+    json_file = open(path, 'w', encoding='utf-8')
+    json_activities.append(activity)
+    dump(json_activities, json_file)
+    json_file.close()
+    return activity
 
 
 def get_all_activities(email):
-    raise Exception('must be implemented')
+    json_file = open(path, 'r', encoding='utf-8')
+    json_activities = load(json_file)
+    json_file.close()
+    return [json_activity for json_activity in json_activities if json_activity['email'] == email]
 
 
-def update_activity(id, user):
-    raise Exception('must be implemented')
+def update_activity(id, activity):
+    json_file = open(path, 'r', encoding='utf-8')
+    json_activities = load(json_file)
+    found = False
+    for json_activity in json_activities:
+        if json_activity['id'] == id and json_activity == activity.email:
+            json_activity['date'] = activity.date
+            activity['event'] = activity.event
+            found = true
+    if not found:
+        raise Exception('no se encontró registro')
+    return activity
 
 
-def delete_activity(id):
-    raise Exception('must be implemented')
+def delete_activity(id, email):
+    json_file = open(path, 'r', encoding='utf-8')
+    json_file.close()
+    json_activities = load(json_file)
+    new_json_activities = [json_activity for json_activity in json_activities if json_activity['id'] not id and json_activity['email'] not email]
+    json_file = open(path, 'w', encoding='utf.8')
+    dump(new_json_activities, json_file)
+    return true
--- src/domain/repositories/user.py
+++ src/domain/repositories/user.py
@@ -1,5 +1,5 @@
 from .repository import Repository
-import json
+from json import load, dump
 from pathlib import Path
 
 
@@ -7,8 +7,8 @@ path = Path(__file__).parent.joinpath('..', 'db', 'user.json')
 
 
 def save(user):
-    json_file = open(User._path, 'r', encoding='utf-8')
-    json_users = json.load(json_file)
+    json_file = open(path, 'r', encoding='utf-8')
+    json_users = load(json_file)
     json_file.close()
     exists = len([json_user for json_user in json_users if json_user['email']
                   == user.email]) > 0
@@ -19,14 +19,14 @@ def save(user):
                 json_user['password'] = user.password
     else:
         json_users.append(self.__dict__)
-    json_file = open(User._path, 'w', encoding='utf-8')
-    json.dump(json_users, fp=json_file)
+    json_file = open(path, 'w', encoding='utf-8')
+    dump(json_users, fp=json_file)
     json_file.close()
     return [json_user for json_user in json_users if json_user['email'] == user.email][0]
 
 
 def get_by_id(id):
-    json_file = open(User._path, 'r', encoding='utf-8')
-    json_users = json.load(json_file)
+    json_file = open(path, 'r', encoding='utf-8')
+    json_users = load(json_file)
     json_file.close()
     return [user for user in json_users if user['email'] == id][0]

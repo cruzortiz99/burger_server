70f1d5363c7400a246cf41ccd4970ed170600f93
Cruz Ortiz <cortiz99>
2019-12-12 02:36:16 -0400

    working on the activities

--- src/routes/activity.py
+++ src/routes/activity.py
@@ -16,18 +16,20 @@ def activities_routes(app):
     '''
     base_url = '/activity'
 
-    @app.route(f'{base_url}', methods=['GET'])
+    @app.route(f'{base_url}/<email>', methods=['GET'])
     def get_all_user_activities():
         return controller.get_all_user_activities('cruzortiz099@gmail.com')
 
     @app.route(f'{base_url}', methods=['POST'])
     def save_activity():
+        request_activity = request.form
         return controller.save_activity()
 
     @app.route(f'{base_url}', methods=['PUT'])
     def update_activity():
+        request_activity = request.form
         return controller.update_activity()
 
-    @app.route(f'{base_url}', methods=['DELETE'])
-    def delete_activity():
-        return controller.delete_activity()
+    @app.route(f'{base_url}/<int:id>/<email>', methods=['DELETE'])
+    def delete_activity(id, email):
+        return controller.delete_activity(id, email)
